bf1bc7bd870b54d94201e46bca24aa86d5ea500b
bndu2002 <110589398+bndu2002@users.noreply.github.com>
2022-09-21 22:20:48 +0530

    Update auth.js

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -2,22 +2,6 @@ const jwt = require("jsonwebtoken")
 //const bookModel = require("../models/booksModel")
 
 
-//const authenticate = async function (req, res, next) {
-  //   try {
-  //     let token = req.headers["x-api-key"] 
-  //     if (!token) return res.status(400).send({ status: false, msg: "token must be present in the request header" })
-  //     req.token = token
-  //     let decodedToken = jwt.verify(token, "Aniket-Subhadeep-Vandana")
-  //     if(!decodedToken)return res.status(401).send({status:false,msg:"unauthanticated user"})//how to send this msg in postman response
-
-  //     //req.decodedToken = decodedToken
-      
-  //   }
-  //   catch (err) {
-  //     res.status(500).send({ msg: err })
-  //   }
-  // }
-
   const authenticate = async function (req, res, next) {
     try {
       let token = req.headers["x-api-key"] 
