aec9e6b2e0684c003c0409d9e3e43d0eac9dd839
bndu2002 <bndusharma2002@gmail.com>
2022-09-24 14:22:15 +0530

    corrected-201(create review),address(validation)

--- src/controllers/reviewController.js
+++ src/controllers/reviewController.js
@@ -1,90 +1,81 @@
 const reviewModel = require('../models/reviewsModel')
-const bookModel = require("../models/booksModel") 
+const bookModel = require("../models/booksModel")
 const mongoose = require('mongoose')
-const { isValidName, isValidRequestBody, isPresent, isValidISBN ,isValidExcerpt} = require('../validator/validator')
+const { isValidName, isValidRequestBody, isPresent } = require('../validator/validator')
 
-const createReview = async function(req,res){
-    try {
-        let bookIdInParam = req.params.bookId
-
-    if (!bookIdInParam) return res.status(400).send({ status: false, message: "Please Provide BookId" })
- 
-    if (!mongoose.isValidObjectId(bookIdInParam)) return res.status(400).send({ status: false, message: "BookId Must Be A Valid ObjectId" })
-  
-    let findBook = await bookModel.findById({_id : bookIdInParam})
-
-    if(!findBook || findBook.isDeleted == true){
-      return res.status(400).send({status:false , message : "Book Does Not Exist Or Is Already Deleted"})
-    }
-
-    let {review, rating, reviewedBy , reviewedAt , bookId} = req.body
-
-    if(!isValidRequestBody(req.body))return res.status(400).send({status:false , message : "Body Cannot Be Empty"})
-
-    
-    
-   if(review){
-    if(!isPresent(review))
-    return res.status(400).send({status : false , message :"Review Cannot Be Empty" })
-   }
-    
-    if(!isPresent(rating))return res.status(400).send({status : false , message : "Ratings Are Required"})
-
-    if(!(/^[1-5]$/).test(rating))return res.status(400).send({status : false , message : "Rating Has To Contain Numbers From 1 to 5"})
-
-    if(!isPresent(reviewedBy)){
-        req.body.reviewedBy = "Guest"
-    }else {
-        if(!isValidName.test(reviewedBy)){
-           return res.status(400).send({status:false , message : "Enter A Valid Reviewer's Name"})
-        }
-    }
-   
-    if(!isPresent(reviewedAt)){
-        req.body.reviewedAt = Date.now()
-    }else {
-        if((!/((\d{4}[\/-])(\d{2}[\/-])(\d{2}))/.test(releasedAt))){
-           return res.status(400).send({status:false , message : "Enter A Valid Date Format"})
-        }
-    }
-
-    if(!isPresent(bookId)){
-        req.body.bookId = bookIdInParam
-    }else {
-        if (!mongoose.isValidObjectId(bookId)) return res.status(400).send({ status: false, message: "BookId Must Be A Valid ObjectId" })
-    }
-
-
-
-    let createReview = await reviewModel.create(req.body)
-    let updateBook = await bookModel.findByIdAndUpdate(
-        {_id : bookIdInParam},
-        {$inc : {reviews : +1}},
-        {new : true}
-    )
-
-    return res.status(200).send({status:true , message : "successfully created" , data : {...updateBook.toObject(),reviewsData :createReview}})
-    
-
-        
-    } catch (error) {
-       return res.status(500).send({status:false , message : error.message})
-    }
-}
-
-const updateReview = async function(req,res){
+const createReview = async function (req, res) {
     try {
         let bookIdInParam = req.params.bookId
 
         if (!bookIdInParam) return res.status(400).send({ status: false, message: "Please Provide BookId" })
-     
+
         if (!mongoose.isValidObjectId(bookIdInParam)) return res.status(400).send({ status: false, message: "BookId Must Be A Valid ObjectId" })
-      
-        let findBook = await bookModel.findById({_id : bookIdInParam})
-        
+
+        let findBook = await bookModel.findById({ _id: bookIdInParam })
+
+        if (!findBook || findBook.isDeleted == true) {
+            return res.status(400).send({ status: false, message: "Book Does Not Exist Or Is Already Deleted" })
+        }
+
+        let { review, rating, reviewedBy, reviewedAt, bookId } = req.body
+
+        if (!isValidRequestBody(req.body)) return res.status(400).send({ status: false, message: "Body Cannot Be Empty" })
+
+        if (review) {
+            if (!isPresent(review))
+                return res.status(400).send({ status: false, message: "Review Cannot Be Empty" })
+        }
+
+        if (!isPresent(rating)) return res.status(400).send({ status: false, message: "Ratings Are Required" })
+
+        if (!(/^[1-5]$/).test(rating)) return res.status(400).send({ status: false, message: "Rating Has To Contain Numbers From 1 to 5" })
+
+        if (!isPresent(reviewedBy)) {
+            req.body.reviewedBy = "Guest"
+        } else {
+            if (!isValidName.test(reviewedBy)) {
+                return res.status(400).send({ status: false, message: "Enter A Valid Reviewer's Name" })
+            }
+        }
+
+        if (!isPresent(reviewedAt)) {
+            req.body.reviewedAt = Date.now()
+        } else {
+            if ((!/((\d{4}[\/-])(\d{2}[\/-])(\d{2}))/.test(releasedAt))) {
+                return res.status(400).send({ status: false, message: "Enter A Valid Date Format" })
+            }
+        }
+
+        if (!isPresent(bookId)) {
+            req.body.bookId = bookIdInParam
+        }
+
+        let createReview = await reviewModel.create(req.body)
+        let updateBook = await bookModel.findByIdAndUpdate(
+            { _id: bookIdInParam },
+            { $inc: { reviews: +1 } },
+            { new: true }
+        )
+
+        return res.status(201).send({ status: true, message: "successfully created", data: { ...updateBook.toObject(), reviewsData: createReview } })
+    } catch (error) {
+        return res.status(500).send({ status: false, message: error.message })
+    }
+}
+
+const updateReview = async function (req, res) {
+    try {
+        let bookIdInParam = req.params.bookId
+
+        if (!bookIdInParam) return res.status(400).send({ status: false, message: "Please Provide BookId" })
+
+        if (!mongoose.isValidObjectId(bookIdInParam)) return res.status(400).send({ status: false, message: "BookId Must Be A Valid ObjectId" })
+
+        let findBook = await bookModel.findById({ _id: bookIdInParam })
+
         console.log(findBook)
-        if(!findBook || findBook.isDeleted == true){
-          return res.status(400).send({status:false , message : "Book Does Not Exist Or Is Already Deleted"})
+        if (!findBook || findBook.isDeleted == true) {
+            return res.status(400).send({ status: false, message: "Book Does Not Exist Or Is Already Deleted" })
         }
 
         let reviewIdinParam = req.params.reviewId
@@ -92,69 +83,65 @@ const updateReview = async function(req,res){
         if (!reviewIdinParam) return res.status(400).send({ status: false, message: "Please Provide ReviewId" })
 
         if (!mongoose.isValidObjectId(reviewIdinParam)) return res.status(400).send({ status: false, message: "ReviewId Must Be A Valid ObjectId" })
-        
-        let findReview = await reviewModel.findById({_id : reviewIdinParam})
-    
-        if(!findReview || findReview.isDeleted == true){
-          return res.status(400).send({status:false , message : "Review Does Not Exist"})
+
+        let findReview = await reviewModel.findById({ _id: reviewIdinParam })
+
+        if (!findReview || findReview.isDeleted == true) {
+            return res.status(400).send({ status: false, message: "Review Does Not Exist" })
         }
 
         //bookA does not have reviewB how to handle
         //fetch book data and compare its id with bookIdInParam
+        if (findReview.bookId != bookIdInParam) return res.status(400).send({ status: false, message: " This Review Does Not Belong To This Book" })
 
-        if(findReview.bookId != bookIdInParam)return res.status(400).send({status:false , message:" This Review Does Not Belong To This Book"})
-        
-        let {review, rating, reviewedBy } = req.body
+        let { review, rating, reviewedBy } = req.body
 
-        if(!isValidRequestBody(req.body))return res.status(400).send({status:false , message : "Body Cannot Be Empty"})
+        if (!isValidRequestBody(req.body)) return res.status(400).send({ status: false, message: "Body Cannot Be Empty" })
 
-        if(review){
-            if(!isPresent(review))
-            return res.status(400).send({status : false , message :"Review Cannot Be Empty" })
-           }
-        
-        if(isPresent(rating)){
-            if(!(/^[1-5]$/).test(rating))return res.status(400).send({status : false , message : "Rating Has To Contain Numbers From 1 to 5"})
+        if (review) {
+            if (!isPresent(review))
+                return res.status(400).send({ status: false, message: "Review Cannot Be Empty" })
         }
 
-        if(reviewedBy){
-            if(!isPresent(reviewedBy)){
+        if (isPresent(rating)) {
+            if (!(/^[1-5]$/).test(rating)) return res.status(400).send({ status: false, message: "Rating Has To Contain Numbers From 1 to 5" })
+        }
+
+        if (reviewedBy) {
+            if (!isPresent(reviewedBy)) {
                 req.body.reviewedBy = 'Guest'
             }
-            if(!isValidName.test(reviewedBy)){
-                return res.status(400).send({status:false , message : "Enter A Valid Reviewer's Name"})
-             }
+            if (!isValidName.test(reviewedBy)) {
+                return res.status(400).send({ status: false, message: "Enter A Valid Reviewer's Name" })
+            }
         }
 
-        
+
         let updateReview = await reviewModel.findByIdAndUpdate(
-            {_id:reviewIdinParam},
+            { _id: reviewIdinParam },
             req.body,
-            {new : true}
+            { new: true }
         )
 
-        return res.status(200).send({status:true , message : "successfully updated" , data : {...findBook.toObject(),reviewsData :updateReview}})
+        return res.status(200).send({ status: true, message: "successfully updated", data: { ...findBook.toObject(), reviewsData: updateReview } })
 
-
-
-        
     } catch (error) {
-        return res.status(500).send({status:false , message : error.message})
+        return res.status(500).send({ status: false, message: error.message })
     }
 }
 
-const deleteReview = async function(req,res){
+const deleteReview = async function (req, res) {
     try {
         let bookIdInParam = req.params.bookId
 
         if (!bookIdInParam) return res.status(400).send({ status: false, message: "Please Provide BookId" })
-     
+
         if (!mongoose.isValidObjectId(bookIdInParam)) return res.status(400).send({ status: false, message: "BookId Must Be A Valid ObjectId" })
-      
-        let findBook = await bookModel.findById({_id : bookIdInParam})
-    
-        if(!findBook || findBook.isDeleted == true){
-          return res.status(400).send({status:false , message : "Book Does Not Exist Or Is Already Deleted"})
+
+        let findBook = await bookModel.findById({ _id: bookIdInParam })
+
+        if (!findBook || findBook.isDeleted == true) {
+            return res.status(400).send({ status: false, message: "Book Does Not Exist Or Is Already Deleted" })
         }
 
         let reviewIdinParam = req.params.reviewId
@@ -162,32 +149,32 @@ const deleteReview = async function(req,res){
         if (!reviewIdinParam) return res.status(400).send({ status: false, message: "Please Provide ReviewId" })
 
         if (!mongoose.isValidObjectId(reviewIdinParam)) return res.status(400).send({ status: false, message: "ReviewId Must Be A Valid ObjectId" })
-        
-        let findReview = await reviewModel.findById({_id : reviewIdinParam})
-    
-        if(!findReview || findReview.isDeleted == true){
-          return res.status(400).send({status:false , message : "Review Does Not Exist Or Is Already Deleted"})
+
+        let findReview = await reviewModel.findById({ _id: reviewIdinParam })
+
+        if (!findReview || findReview.isDeleted == true) {
+            return res.status(400).send({ status: false, message: "Review Does Not Exist Or Is Already Deleted" })
         }
-        
-        if(findReview.bookId != bookIdInParam)return res.status(400).send({status:false , message:" This Review Does Not Belong To This Book"})
-       
+
+        if (findReview.bookId != bookIdInParam) return res.status(400).send({ status: false, message: " This Review Does Not Belong To This Book" })
+
         let updateReview = await reviewModel.findByIdAndUpdate(
-            {_id :reviewIdinParam },
-            {$set : {isDeleted : true }},
-            {new : true}
+            { _id: reviewIdinParam },
+            { $set: { isDeleted: true } },
+            { new: true }
         )
 
         let updateBook = await bookModel.findByIdAndUpdate(
-            {_id:bookIdInParam},
-            {$inc : {reviews : -1}},
-            {new : true}
+            { _id: bookIdInParam },
+            { $inc: { reviews: -1 } },
+            { new: true }
         )
 
-        return res.status(200).send({status:true , message : "successfully deleted"})
-        
+        return res.status(200).send({ status: true, message: "successfully deleted" })
+
     } catch (error) {
-        return res.status(500).send({status:false , message : error.message})
+        return res.status(500).send({ status: false, message: error.message })
     }
 }
 
-module.exports = {createReview,updateReview,deleteReview}
+module.exports = { createReview, updateReview, deleteReview }
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,88 +1,97 @@
 const userModel = require("../models/usersModel")
-const{isValidName , isValidRequestBody , isPresent , isValidMail ,isValidNumber , isvalidPassword} = require('../validator/validator')
-const jwt  = require('jsonwebtoken')
+const { isValidName, isValidRequestBody, isPresent, isValidMail, isValidNumber, isvalidPassword } = require('../validator/validator')
+const jwt = require('jsonwebtoken')
 
-const createUser = async function(req,res){
- try {
-    let {title , name , phone ,email , password , address} = req.body 
+const createUser = async function (req, res) {
+    try {
+        let { title, name, phone, email, password, address } = req.body
 
-    if(!isValidRequestBody(req.body))return res.status(400).send({status:false , message : "body can't be empty"})
-    
-    if(!isPresent(title))return res.status(400).send({status:false , message : "title must be present"})
+        if (!isValidRequestBody(req.body)) return res.status(400).send({ status: false, message: "body can't be empty" })
 
-    if(!title.includes("Mr", "Mrs", "Miss"))return res.status(400).send({status:false , message : "title is invalid"})
+        if (!isPresent(title)) return res.status(400).send({ status: false, message: "title must be present" })
 
-    if(!isPresent(name))return res.status(400).send({status:false , message : "name must be present"})
+        if (!title.includes("Mr", "Mrs", "Miss")) return res.status(400).send({ status: false, message: "title is invalid" })
 
-    if(!isValidName.test(name))return res.status(400).send({status:false , message : "name is invalid"})
+        if (!isPresent(name)) return res.status(400).send({ status: false, message: "name must be present" })
 
-    if(!isPresent(phone))return res.status(400).send({status:false , message : "phone must be present"})
+        if (!isValidName.test(name)) return res.status(400).send({ status: false, message: "name is invalid" })
 
-    if(!isValidNumber.test(phone))return res.status(400).send({status:false , message : "phone is invalid"})
+        if (!isPresent(phone)) return res.status(400).send({ status: false, message: "phone must be present" })
 
-    let checkNumber = await userModel.findOne({phone:phone})
+        if (!isValidNumber.test(phone)) return res.status(400).send({ status: false, message: "phone is invalid" })
 
-    if(checkNumber)return res.status(400).send({status:false , message : "phone number already exists"})
+        let checkNumber = await userModel.findOne({ phone: phone })
 
-    if(!isPresent(email))return res.status(400).send({status:false , message : "email must be present"})
+        if (checkNumber) return res.status(400).send({ status: false, message: "phone number already exists" })
 
-    if(!isValidMail.test(email))return res.status(400).send({status:false , message : "email is invalid"})
+        if (!isPresent(email)) return res.status(400).send({ status: false, message: "email must be present" })
 
-    let checkEmail = await userModel.findOne({email:email})
+        if (!isValidMail.test(email)) return res.status(400).send({ status: false, message: "email is invalid" })
 
-    if(checkEmail)return res.status(400).send({status:false , message : "email is already registerd"})
+        let checkEmail = await userModel.findOne({ email: email })
 
-    if(!isPresent(password))return res.status(400).send({status:false , message : "password must be present"})
+        if (checkEmail) return res.status(400).send({ status: false, message: "email is already registerd" })
 
-    if(!isvalidPassword.test(password))return res.status(400).send({status:false , message : "password must contain 8 to 15 charactors"})
+        if (!isPresent(password)) return res.status(400).send({ status: false, message: "password must be present" })
 
-    let createUser = await userModel.create(req.body)
+        if (!isvalidPassword.test(password)) return res.status(400).send({ status: false, message: "password must contain 8 to 15 charactors" })
 
-    return  res.status(201).send({status:true , message : "successfully created" , data : createUser})
+        if (address) {
+            if (!address === Object || Object.keys(address).length == 0)
+                return res.status(400).send({ status: false, message: "address has to contain key-value pair" })
+            if (address.pincode) {
+                if (!(/^\d{3}\s?\d{3}$/.test(address.pincode)))
+                    return res.status(400).send({ status: false, message: "Enter A Valid Pincode" })
+            }
+        }
 
-} catch (error) {
-   return res.status(500).send({status:false , message : error.message})
- }
-   
+        let createUser = await userModel.create(req.body)
+
+        return res.status(201).send({ status: true, message: "successfully created", data: createUser })
+
+    } catch (error) {
+        return res.status(500).send({ status: false, message: error.message })
+    }
+
 }
 
 
-const loginUser = async function(req,res){
+const loginUser = async function (req, res) {
     try {
-        let {email,password} = req.body
+        let { email, password } = req.body
 
-        if(!isValidRequestBody(req.body))return res.status(400).send({status:false , message : "body cannot be empty"})
-        
-        if(!isPresent(email))return res.status(400).send({status:false, message : "email is required"})
-        
-        if(!isPresent(password))return res.status(400).send({status:false, message : "password is required"})
-        
-        let findUser = await userModel.findOne({email:email , password:password})
-        
-        if(!findUser)return res.status(404).send({status:false , message: "User Not Found"})
+        if (!isValidRequestBody(req.body)) return res.status(400).send({ status: false, message: "body cannot be empty" })
+
+        if (!isPresent(email)) return res.status(400).send({ status: false, message: "email is required" })
+
+        if (!isPresent(password)) return res.status(400).send({ status: false, message: "password is required" })
+
+        let findUser = await userModel.findOne({ email: email, password: password })
+
+        if (!findUser) return res.status(404).send({ status: false, message: "User Not Found" })
 
         let token = jwt.sign(
             {
-                userId : findUser._id.toString(),
-                groupNumber : 54,
-                organisation : "FunctionUP",
+                userId: findUser._id.toString(),
+                groupNumber: 54,
+                organisation: "FunctionUP",
                 iat: Math.floor(new Date() / 1000),
-                
+
             },
             "Aniket-Subhadeep-Vandana",
-            {expiresIn : '30s'}
-        
+            { expiresIn: '1h' }
+            //change "expiresIn" : '10s' ,to check the jwt expiration
         )
 
-        //how to get exp and iat in response ??
-        req.header('x-api-key',token)
-        res.status(200).send({status:true , message : "success" ,data: token})
+        
+        req.header('x-api-key', token)
+        res.status(200).send({ status: true, message: "success", data: token })
     } catch (error) {
-        res.status(500).send({status:false , message : error.message})
+        res.status(500).send({ status: false, message: error.message })
     }
 }
 
-module.exports = {createUser,loginUser}
+module.exports = { createUser, loginUser }
 
 
 
