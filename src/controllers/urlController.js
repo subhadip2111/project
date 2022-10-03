94e5bbab7b25f6d28409089de41147b9fbf4ecac
asifrazaansari <asifraza0231@gmail.com>
2022-10-03 16:31:09 +0530

    added cache and redis

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -2,6 +2,25 @@ const validUrl = require('valid-url')
 const shortid = require('shortid')
 const Url = require('../models/urlModel')
 
+const redis = require("redis");
+const { promisify } = require("util");
+
+const redisClient = redis.createClient(
+   14026,
+  "redis-14026.c17.us-east-1-4.ec2.cloud.redislabs.com",
+  { no_ready_check: true }
+);
+redisClient.auth("9orH2ONkwQY0JzqcxXD118kxuDJw9x0c", function (err) {
+  if (err) throw err;
+});
+
+redisClient.on("connect", async function () {
+  console.log("Connected to Redis..");
+});
+
+const SET_ASYNC = promisify(redisClient.SET).bind(redisClient);
+const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
+
 const shortenUrl = async (req, res) => {
     const baseUrl = 'http:localhost:3000'
     const { longUrl } = req.body
@@ -37,9 +56,17 @@ const originUrl = async function (req, res) {
     try {
         let urlCode = req.params.urlCode
         urlCode = urlCode.toLowerCase()
-        const data = await Url.findOne({ urlCode: urlCode })
-        if (!data) return res.status(404).send({ status: false, message: "Url not found" })
-        return res.status(302).redirect(data.longUrl)
+
+        let cachedURL = await GET_ASYNC(`${urlCode}`)
+        if(cachedURL){
+            return res.status(302).redirect(cachedURL)
+        }else{
+            const data = await Url.findOne({ urlCode: urlCode })
+            if (!data) return res.status(404).send({ status: false, message: "Url not found" })
+            await SET_ASYNC(`${urlCode}`, data.longUrl)
+            return res.status(302).redirect(data.longUrl)
+        }
+       
     }
     catch (error) {
         return res.status(500).send({ status: false, message: error.message })
